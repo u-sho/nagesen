cb786c3c124ad7d51a5b7dec1798fd23b5932b7c
@redimpulz.s.uechi <s.uechi@redimpulz.com>
2019-03-14 20:13:18 +0900

    created first page

---
+++ vuesplash/resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Roboto:400">
+  <link rel="stylesheet" href="https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css">
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+  <div id="app"></div>
+  <script src="{{ mix('js/app.js') }}" defer></script>
+</body>
+</html>
--- vuesplash/routes/web.php
+++ vuesplash/routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('nagesen')->group(function () {
+    Route::get('/{path}', function () {
+        return view('index');
+    })->where('path', '.*');
 });
